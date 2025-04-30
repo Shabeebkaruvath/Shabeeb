79e1fda866009b91c2c35e32f3a8b20adf6324c5
Shabeebkaruvath <justshabeeb@gmail.com>
2025-04-30 20:45:05 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
